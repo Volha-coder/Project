2422a582158e187a0bbffd9bb54e1ada69a6ef1f
Volha-coder <ibicca_88@mail.ru>
2019-11-10 23:56:07 +0300

    Worked sourses

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,12 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
+    var name = document.getElementById('name').value;
+    var message = document.getElementById('message').value;
+    var email = document.getElementById('email').value;
+        $('#exampleModal').on('show.bs.modal', function (event) {
+            var modal = $(this);
+            modal.find('.first-name input').val(name);
+            modal.find('.mail input').val(email);
+            modal.find('.message textarea').val(message);
+        })
 
-
-$('#exampleModal').on('show.bs.modal', function (event) {
-
-    var name = document.getElementById("name").value; // Extract info from data-* attributes
-    var message = document.getElementById("message").value;
-    var mail = document.getElementById("email").value;
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this);
-    modal.find('.first-name input').val(name);
-    modal.find('.mail input').val(mail);
-    modal.find('.message textarea').val(message)
-})
-$("#navbar a").on('click', function(event) {
-  
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-  
-      // Prevent default anchor click behavior
-      event.preventDefault();
-  
-      // Store hash
-      var hash = this.hash;
-  
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-  
-      // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-  
-    } // End if
-  
-  });
